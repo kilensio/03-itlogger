9451d5ecf8ad51be2dc48ab5525c433269be8064
Kilensio <akilensio@gmail.com>
2021-07-25 16:56:53 +0300

    Redux for logs (Get, Add, Edit, Search, Delete)

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { connect } from 'react-redux'
+import { searchLogs } from '../../actions/logActions'
+import PropTypes from 'prop-types'
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef('')
+
+  const onChange = e => {
+    searchLogs(text.current.value)
+  }
+  // const onChange = e => {
+  //   searchLogs(e.target.value.trim())
+  // }
+
   return (
     <nav style={{ marginBottom: '30px' }} className="cyan">
     <div className="nav-wrapper">
       <form>
         <div className="input-field">
-          <input id="search" type="search" />
+          <input 
+            id="search" 
+            type="search" 
+            placeholder='Search Logs...'
+            ref={text}
+            onChange={onChange}
+          />
           <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
           <i className="material-icons">close</i>
         </div>
@@ -16,4 +34,8 @@ const SearchBar = () => {
   )
 }
 
-export default SearchBar
+SearchBar.propTypes = {
+  searchLogs: PropTypes.func.isRequired
+}
+
+export default connect(null, { searchLogs })(SearchBar)
